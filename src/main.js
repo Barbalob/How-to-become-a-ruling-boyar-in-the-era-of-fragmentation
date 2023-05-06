4855ef16d7178e978c7d24bd62c7154ce29b22d8
lenlis <sit.a.v@mail.ru>
2023-05-06 20:50:55 +0500

    тестовый вариант книги из демо

--- src/main.js
+++ src/main.js
@@ -2,11 +2,55 @@
 import './styles/style.scss'
 
 import {PageFlip} from 'page-flip';
-const pageFlip = new PageFlip(document.getElementById('book'),
-    {
-        width: 701, // required parameter - base page width
-        height: 878,  // required parameter - base page height
-    }
-);
 
-pageFlip.loadFromHTML(document.querySelectorAll('.page'));
+document.addEventListener('DOMContentLoaded', function() {
+
+    const pageFlip = new PageFlip(
+        document.getElementById("demoBookExample"),
+        {
+            width: 550, // base page width
+            height: 733, // base page height
+
+            size: "stretch",
+            // set threshold values:
+            minWidth: 315,
+            maxWidth: 1000,
+            minHeight: 420,
+            maxHeight: 1350,
+            drawShadow: false,
+            showCover: true,
+            mobileScrollSupport: false // disable content scrolling on mobile devices
+        }
+    );
+
+    // load pages
+    pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+    document.querySelector(".page-total").innerText = pageFlip.getPageCount();
+    document.querySelector(
+        ".page-orientation"
+    ).innerText = pageFlip.getOrientation();
+
+    document.querySelector(".btn-prev").addEventListener("click", () => {
+        pageFlip.flipPrev(); // Turn to the previous page (with animation)
+    });
+
+    document.querySelector(".btn-next").addEventListener("click", () => {
+        pageFlip.flipNext(); // Turn to the next page (with animation)
+    });
+
+    // triggered by page turning
+    pageFlip.on("flip", (e) => {
+        document.querySelector(".page-current").innerText = e.data + 1;
+    });
+
+    // triggered when the state of the book changes
+    pageFlip.on("changeState", (e) => {
+        document.querySelector(".page-state").innerText = e.data;
+    });
+
+    // triggered when page orientation changes
+    pageFlip.on("changeOrientation", (e) => {
+        document.querySelector(".page-orientation").innerText = e.data;
+    });
+});
