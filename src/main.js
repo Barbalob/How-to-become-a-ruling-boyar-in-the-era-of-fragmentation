a0d4871342aaa32b4b548aee945f772ea6dcb132
lenlis <sit.a.v@mail.ru>
2023-05-06 19:02:57 +0500

    тестовая книга

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 // import name from "./module.js";
 import './styles/style.scss'
+
+import {PageFlip} from 'page-flip';
+const pageFlip = new PageFlip(document.getElementById('book'),
+    {
+        width: 701, // required parameter - base page width
+        height: 878,  // required parameter - base page height
+    }
+);
+
+pageFlip.loadFromHTML(document.querySelectorAll('.page'));
