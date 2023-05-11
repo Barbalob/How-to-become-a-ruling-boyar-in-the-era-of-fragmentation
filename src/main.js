2a86ce4b2d1d3099457f6ae26f2ce907ce294761
Barbalob <a79221932005@gmail.com>
2023-05-11 16:34:53 +0500

    1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 // import name from "./module.js";
 import './styles/style.scss'
-
+const buttonStart = document.querySelector('#Start') 
+const leftPage = document.querySelector('.page__left')
+const rightPage = document.querySelector('.page__right')
+buttonStart.addEventListener('click', () => {
+    leftPage.remove();
+    rightPage.remove();
+})
