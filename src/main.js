83d6ba1ff1effa601dc7528a9b50714a237ed6f2
Barbalob <a79221932005@gmail.com>
2023-05-11 21:39:01 +0500

    добавил стили на вторую страницу

--- src/main.js
+++ src/main.js
@@ -2,53 +2,53 @@
 import { PageFlip } from 'page-flip'
 import './styles/style.scss'
 const buttonStart = document.querySelector('#start') 
-const leftPage = document.querySelector('.page1__left')
-const rightPage = document.querySelector('.page1__right')
-const mainPage = document.querySelector('.page1')
+const leftPage = document.querySelector('.menu__left')
+const rightPage = document.querySelector('.menu__right')
+const mainPage = document.querySelector('.menu')
 buttonStart.addEventListener('click', () => {
     leftPage.remove();
     rightPage.remove();
     mainPage.innerHTML = `
     <div class="container">
-    <div class="flip-book" id="demoBookExample">
-        <div class="page page-cover page-cover-top page-test" data-density="hard">
-            <div class="page-content">
-              <div class="imge">1</div>
-                <h2>BOOK TITLE</h2>
+          <div class="wrapper-book">
+            <div class="flip-book" id="demoBookExample">
+                <div class="page page-cover page-cover-top page-test" data-density="hard">
+                    <div class="page-content">
+                        <h2>BOOK TITLE</h2>
+                    </div>
+                </div>
+                <div class="page page-test2">
+                    <div class="page-content">
+                        <h2 class="page-header">Page header 1</h2>
+                        <div class="page-image" style="background-image: url(images/html/1.jpg)"></div>
+                        <div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.</div>
+                        <div class="page-footer">2</div>
+                    </div>
+                </div>
+                <div class="page page-test2">
+                    <div class="page-content ">
+                        <h2 class="page-header">Page header - 15</h2>
+                        <div class="page-image" style="background-image: url(images/html/7.jpg)"></div>
+                        <div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.</div>
+                        <div class="page-footer">16</div>
+                    </div>
+                </div>
+                <div class="page page-test2">
+                    <div class="page-content">
+                        <h2 class="page-header">Page header - 16</h2>
+                        <div class="page-image" style="background-image: url(images/html/8.jpg)"></div>
+                        <div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.</div>
+                        <div class="page-footer">17</div>
+                    </div>
+                </div>
+                <div class="page page-cover page-cover-bottom" data-density="hard">
+                    <div class="page-content">
+                        <h2>THE END</h2>
+                    </div>
+                </div>
             </div>
+          </div>
         </div>
-        <div class="page page-test2">
-            <div class="page-content">
-                <h2 class="page-header">Page header 1</h2>
-                <div class="page-image" style="background-image: url(images/html/1.jpg)"></div>
-                <div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.</div>
-                <div class="page-footer">2</div>
-            </div>
-        </div>
-        <!-- PAGES .... -->
-        <div class="page page-test2">
-            <div class="page-content ">
-                <h2 class="page-header">Page header - 15</h2>
-                <div class="page-image" style="background-image: url(images/html/7.jpg)"></div>
-                <div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.</div>
-                <div class="page-footer">16</div>
-            </div>
-        </div>
-        <div class="page page-test2">
-            <div class="page-content">
-                <h2 class="page-header">Page header - 16</h2>
-                <div class="page-image" style="background-image: url(images/html/8.jpg)"></div>
-                <div class="page-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus, accumsan eros sed, viverra enim. Pellentesque non justo vel nibh sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus mollis nibh, non convallis ex convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat. Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra metus, a venenatis tellus tellus id magna.</div>
-                <div class="page-footer">17</div>
-            </div>
-        </div>
-        <div class="page page-cover page-cover-bottom" data-density="hard">
-            <div class="page-content">
-                <h2>THE END</h2>
-            </div>
-        </div>
-    </div>
-</div>
 `
 const pageFlip = new PageFlip(
     document.getElementById("demoBookExample"),
@@ -71,6 +71,8 @@ const pageFlip = new PageFlip(
 
 // load pages
 pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+
 
 // document.querySelector(".page-total").innerText = pageFlip.getPageCount();
 // document.querySelector(
@@ -100,4 +102,3 @@ pageFlip.loadFromHTML(document.querySelectorAll(".page"));
 //     document.querySelector(".page-orientation").innerText = e.data;
 // });
 })
-
