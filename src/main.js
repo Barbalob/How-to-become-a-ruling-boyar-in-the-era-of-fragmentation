640b52414f9cb81b91c22913201685172ba17aff
Barbalob <a79221932005@gmail.com>
2023-05-11 16:50:00 +0500

    Добавление Книги при нажатии кнопки

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 // import name from "./module.js";
 import './styles/style.scss'
-const buttonStart = document.querySelector('#Start') 
+const buttonStart = document.querySelector('#start') 
 const leftPage = document.querySelector('.page__left')
 const rightPage = document.querySelector('.page__right')
+const mainPage = document.querySelector('.page')
 buttonStart.addEventListener('click', () => {
     leftPage.remove();
     rightPage.remove();
+    mainPage.innerHTML = `
+    <div class="real_book">
+    <div class="book_page">
+        test
+    </div>
+    <div class="book_page">
+
+    </div>
+    <div class="book_page">
+
+    </div>
+    <div class="book_page">
+
+    </div>
+    </div>
+`
 })
+
