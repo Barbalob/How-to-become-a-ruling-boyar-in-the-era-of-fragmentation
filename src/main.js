b3450b5a5e40f5edd422fc4c6c7c9b88c868a357
Barbalob <a79221932005@gmail.com>
2023-06-07 18:00:31 +0500

    build

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ buttonStart.addEventListener('click', () => {
             pOne.classList.remove("right-br")
             pTwo.classList.remove("left-br")
         });
-        sleep(1500).then(() => {
+        sleep(1300).then(() => {
             pageFlip.destroy();
             const container1 = document.querySelector('.container1');
             container1.remove();
@@ -276,91 +276,99 @@ buttonStart.addEventListener('click', () => {
                                 buttonChoice.disabled = true;
                             }
                         }
-                        CreateListBook(`
-                        <li class="page">
-                        <div class="page-content content-img">
-                            <figure>
-                                <img class="content-img" src="assets/image/4.png" alt="Съезд князей">
-                                <figcaption class="text-img-author text-medium">Отравление Ростислава Владимировича Тмутараканского, первого князя-изгоя, греческим стратигом. Миниатюра из Радзивилловской летописи (XV век)</figcaption>                     
-                            </figure>                       
-                            <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
-                        </div>
-                        </li>    
 
-                        <li class="page">
-                            <div class="page-content content-text">
-                                <div class='main-text'> 
-                                    <img src="assets/letter/letterH.svg" alt="Буква И" class='img-letter-small'>
-                                    <div class='text text-small'> 
-                                    Отправив гонцов к ближайшим
-                                    князьям, Вы либо не получаете
-                                    никаких известий, либо получаете послания с отказами в помощи. 
-                                    Еще бы: кому нужно поддерживать молодого князя-изгоя, да еще и без большого воинства! 
-                                    А вот к западу от Русских земель такие силы как раз имеются. Силы, готовые за обещания земель или золота, помочь заполучить Вашему князю вожделенный престол. 
-                                    Вы отправляйтесь в Польшу и прибываете ко двору польского короля из династии Пястов, который довольно сносно Вас принимает, но, стоит отметить, без особого энтузиазма.
-                                    </div>
+                        if (id == 1){
+                            CreateListBook(`
+                                <li class="page">
+                                <div class="page-content content-img">
+                                    <figure>
+                                        <img class="content-img" src="assets/image/4.png" alt="Съезд князей">
+                                        <figcaption class="text-img-author text-medium">Отравление Ростислава Владимировича Тмутараканского, первого князя-изгоя, греческим стратигом. Миниатюра из Радзивилловской летописи (XV век)</figcaption>                     
+                                    </figure>                       
+                                    <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
                                 </div>
-                                <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>
-                            </div>
-                        </li>
-                    `)
-                        CreateListBook(` 
+                                </li>    
 
-                        <li class="page">
-                            <div class="page-content content-text">
-                                <div class='main-text'> 
-                                    <img src="assets/letter/letterP.svg" alt="Буква П" class='img-letter-small'>
-                                    <div class='text text-small'> 
-                                    рибыв ко двору польского короля из
-                                    династии Пястов, Вы очаровываите
-                                    монарха и придворных своим знанием языка и обычаев. Польский король выделяет Вам и князю лучшие покои в своем замке, а также положительно отвечает на Ваше предложение о военном союзе. 
-                                    <br>
-                                    Впечатленный Вашим дипломатическим даром и даром толмача, князь не может сдержать своего восхищения.
-                                    После аудиенции у польского короля Вы возвращайтесь в свои покои, куда Вам приносят княжеский вышитый пояс в качестве знака высшей признательности. 
-                                    Так, Вы не просто завоевали уважение своего князя, но и снискали его милость, что, конечно же, не применет сказаться на Вашем статусе и положении как при иноземных дворах, так и при будущем дворе Вашего князя.
+                                <li class="page">
+                                    <div class="page-content content-text">
+                                        <div class='main-text'> 
+                                            <img src="assets/letter/letterH.svg" alt="Буква И" class='img-letter-small'>
+                                            <div class='text text-small'> 
+                                            Отправив гонцов к ближайшим
+                                            князьям, Вы либо не получаете
+                                            никаких известий, либо получаете послания с отказами в помощи. 
+                                            Еще бы: кому нужно поддерживать молодого князя-изгоя, да еще и без большого воинства! 
+                                            А вот к западу от Русских земель такие силы как раз имеются. Силы, готовые за обещания земель или золота, помочь заполучить Вашему князю вожделенный престол. 
+                                            Вы отправляйтесь в Польшу и прибываете ко двору польского короля из династии Пястов, который довольно сносно Вас принимает, но, стоит отметить, без особого энтузиазма.
+                                            </div>
+                                        </div>
+                                        <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>
                                     </div>
-                                </div>
-                                <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>
-                            </div>
-                        </li>
-
-                        <li class="page">
-                        <div class="page-content content-img">
-                            <figure>
-                                <h2>Получен трофей:</h2>
-                                <img class="content-img" src="assets/trophy/1.png" alt="Княжеский вышитый пояс">
-                                <figcaption class="text-img-author text-medium">Княжеский вышитый пояс</figcaption>                     
-                            </figure>                       
-                            <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
-                        </div>
-                        </li>   
-                    `)
-                       
-                        CreateListBook(`
-                        <li class="page">
-                        <div class="page-content content-img">
-                            <figure>
-                                <img class="content-img" src="assets/image/4.png" alt="Съезд князей">
-                                <figcaption class="text-img-author text-medium">Отравление Ростислава Владимировича Тмутараканского, первого князя-изгоя, греческим стратигом. Миниатюра из Радзивилловской летописи (XV век)</figcaption>                     
-                            </figure>                       
-                            <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
-                        </div>
-                        </li>    
-
-                        <li class="page">
-                            <div class="page-content content-text">
-                                <div class='main-text'> 
-                                    <img src="assets/letter/letterB.svg" alt="Буква И" class='img-letter-small'>
-                                    <div class='text text-small'> 
-                                    ы не захотели прожить 
-                                    по-настоящему увлекательную и
-                                    полную приключений жизнь. Вы умерли в безвестности, и не один летописец не счел нужным упомянуть о Вас хотя бы вкратце.
+                                </li>
+                            `)
+                        }
+                        if (id == 2){
+                            CreateListBook(` 
+                                <li class="page">
+                                    <div class="page-content content-text">
+                                        <div class='main-text'> 
+                                            <img src="assets/letter/letterP.svg" alt="Буква П" class='img-letter-small'>
+                                            <div class='text text-small'> 
+                                            рибыв ко двору польского короля из
+                                            династии Пястов, Вы очаровываите
+                                            монарха и придворных своим знанием языка и обычаев. Польский король выделяет Вам и князю лучшие покои в своем замке, а также положительно отвечает на Ваше предложение о военном союзе. 
+                                            <br>
+                                            <br>
+                                            Впечатленный Вашим дипломатическим даром и даром толмача, князь не может сдержать своего восхищения.
+                                            После аудиенции у польского короля Вы возвращайтесь в свои покои, куда Вам приносят княжеский вышитый пояс в качестве знака высшей признательности. 
+                                            Так, Вы не просто завоевали уважение своего князя, но и снискали его милость, что, конечно же, не применет сказаться на Вашем статусе и положении как при иноземных дворах, так и при будущем дворе Вашего князя.
+                                            </div>
+                                        </div>
+                                        <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>
                                     </div>
+                                </li>
+        
+                                <li class="page">
+                                <div class="page-content content-img">
+                                    <figure>
+                                        <h2 class='text-trophy'>Получен трофей:</h2>
+                                        <img class="content-img" src="assets/trophy/1.png" alt="Княжеский вышитый пояс">
+                                        <figcaption class="text-img-author text-medium">Княжеский вышитый пояс</figcaption>                     
+                                    </figure>                       
+                                    <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
                                 </div>
-                                <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>
-                            </div>
-                        </li>
-                    `)
+                                </li>   
+                            `)
+                        }
+                        if (id == 3){
+                            CreateListBook(`
+                                <li class="page">
+                                <div class="page-content content-img">
+                                    <figure>
+                                        <img class="content-img" src="assets/image/4.png" alt="Съезд князей">
+                                        <figcaption class="text-img-author text-medium">Отравление Ростислава Владимировича Тмутараканского, первого князя-изгоя, греческим стратигом. Миниатюра из Радзивилловской летописи (XV век)</figcaption>                     
+                                    </figure>                       
+                                    <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
+                                </div>
+                                </li>    
+        
+                                <li class="page">
+                                    <div class="page-content content-text">
+                                        <div class='main-text'> 
+                                            <img src="assets/letter/letterB.svg" alt="Буква И" class='img-letter-small'>
+                                            <div class='text text-small'> 
+                                            ы не захотели прожить 
+                                            по-настоящему увлекательную и
+                                            полную приключений жизнь. Вы умерли в безвестности, и не один летописец не счел нужным упомянуть о Вас хотя бы вкратце.
+                                            </div>
+                                        </div>
+                                        <button id="choice" class="menu__button text-medium" onclick="location.reload(); return false;">Начать игру заново</button>
+                                        <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>
+                                    </div>
+                                </li>
+                            `)
+                        }
+  
                         sleep(500).then(() => {
                             testElement.remove();
                             container.classList.add('move-right')
