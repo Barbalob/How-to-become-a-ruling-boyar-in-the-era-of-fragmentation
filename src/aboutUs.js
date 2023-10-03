b6a53da3dc43ff4f3f39f2cb3995b7b5d1e4dc9b
lenlis <sit.a.v@mail.ru>
2023-10-03 13:15:19 +0500

    изменен порядок имен

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -1,22 +1,22 @@
 const people = {
-    'Сергей Рябов': ['Sergey.jpg','Руководитель проекта, куратор и сценарист'],
-    'Алексей Попович': ['Alexei.jpg','Редактор и научный консультант проекта'],
-    'Кузнецова Мария': ['Maria.jpg','Тимлид'],
-    'Федорова Елизавета': ['Lisa.jpg','Дизайнер'],
-    'Кунщиков Артём': ['Artem.jpg','Frontend'],
-    'Ситников Андрей': ['Andrey.jpg','Frontend'],
+    'Рябов Сергей': ['Sergey.jpg','Руководитель проекта, куратор и сценарист'],
+    'Попович Алексей': ['Alexei.jpg','Редактор и научный консультант проекта'],
+    'Мария Кузнецова': ['Maria.jpg','Тимлид'],
+    'Елизавета Федорова': ['Lisa.jpg','Дизайнер'],
+    'Артём Кунщиков': ['Artem.jpg','Frontend'],
+    'Андрей Ситников': ['Andrey.jpg','Frontend'],
 };
 
 export const aboutUs = 
   `
     ${
         Object.keys(people).map(key =>{
-            console.log(key);
+            // console.log(key);
             return `
             <li class="page">
                 <div class="page-content content-img">
                     <figure>
-                        <img class="content-img" src="assets/photo/${people[key][0]}" alt="Портрет Гоголя">
+                        <img class="content-img" src="assets/photo/${people[key][0]}" alt="${key}">
                         <figcaption class="text-img-author text-medium">${key}<br>${people[key][1]}</figcaption>                     
                     </figure>                       
                     <div class='decor'><img src="assets/decor-1.svg" alt="" class="img-decor"></div>    
